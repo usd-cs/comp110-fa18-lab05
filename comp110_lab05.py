68f5d774390551c697356f97fb17bfa2bfc5aada
Sat Garcia <sat@sandiego.edu>
2018-10-03 13:10:04 -0700

    initial files

---
+++ comp110_lab05.py
@@ -0,0 +1,35 @@
+"""
+Module: comp110_lab05
+
+Modules with some functions for Lab 05 practice problems.
+"""
+
+def caesar_encrypt(str_plain, shift_amount):
+    """
+    Uses the Caesar cipher with shift amount shift_amount to encrypt the 
+    string str_plain, and returns that string.
+    """
+
+    str_cipher = ""
+
+    return 0  # placeholder.  Replace the 0.
+
+def caesar_decrypt(str_cipher, shift_amount):
+    """
+    Uses the Caesar cipher with shift amount shift_amount to decrypt the 
+    string str_cipher, and returns that string.
+    """
+
+    str_plain = ""
+
+    return 0   # Placeholder.  Replace the 0
+
+def modify_list(in_list):
+    """
+    Returns a copy of the list in_list in which all entries
+    that lie between a 5 and a 10 have been removed.
+    """
+
+    out_list = []
+
+    return 0 # placeholder.  Replace the 0
